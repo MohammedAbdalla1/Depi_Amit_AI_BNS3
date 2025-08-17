bcbdd06025042b26a0e97c383154096230503177
Mohamed-Abdalla <mohamed.a.afifi.work@gmail.com>
2025-08-17 16:15:32 +0300

    commiting database folder

---
+++ sources/database/app.py
@@ -0,0 +1,75 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
+from PyQt5 import uic
+import sys
+import psycopg2
+
+
+class Main_App(QMainWindow):
+    def __init__(self):
+        super(Main_App,self).__init__()
+        uic.loadUi(r"C:\Users\Maydoum\OneDrive\Desktop\DEPI\projects\Depi_Amit_AI_BNS3\sources\database\std.ui", self)
+        self.tabWidget.tabBar().setVisible(False)
+        self.InitUI()
+        self.handle_btn()
+        self.handle_db_conn()   # <-- added db connection here
+
+    def InitUI(self):
+        self.setWindowTitle("Student System")
+
+    def handle_db_conn(self):
+        try:
+            self.db = psycopg2.connect(
+                host="localhost",
+                database="student_mgm",
+                user="postgres",
+                password="1475369",   # <-- put your postgres password here
+                port="5432"
+            )
+            self.curr = self.db.cursor()
+            print("Connection is Done!")
+        except Exception as e:
+            QMessageBox.critical(self, "Database Error", str(e))
+            sys.exit(1)
+
+    def handle_btn(self):
+        ############################student_btn#######################
+        self.std_add_btn.clicked.connect(self.add_std_info)
+        self.std_update_btn.clicked.connect(self.update_std_info)
+        self.std_del_btn.clicked.connect(self.del_std_info)
+       
+        ######################## Enrollment btn  #################################
+       
+        # self.std_add_btn.clicked.connect(self.add_std_info)
+        # self.std_update_btn.clicked.connect(self.update_std_info)
+        # self.std_del_btn.clicked.connect(self.del_std_info)
+       
+    ###################################################################      
+       
+    def add_std_info(self):
+        std_id = self.std_Id_txt.text()
+        std_name = self.std_name_txt.text()
+        std_email = self.std_email_txt.text()
+        std_phone = self.std_phone_txt.text()
+        try:
+            self.curr.execute(
+                "INSERT INTO students (student_id, name, email, phone) values(%s,%s,%s,%s)",
+                (std_id, std_name, std_email, std_phone)
+            )
+            self.db.commit()
+            QMessageBox.information(self, "Success", "Student added successfully!")
+        except Exception as e:
+            self.db.rollback()
+            QMessageBox.critical(self, "Error", str(e))
+
+    def update_std_info(self):
+        print('welcome to my app')
+
+    def del_std_info(self):
+        print('welcome to my app')    
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = Main_App()
+    window.show()
+    sys.exit(app.exec_())
